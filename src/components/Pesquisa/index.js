ab975c786249737bab0139d035d42bac0b069102
Hugov70 <145301917+Hugov70@users.noreply.github.com>
2024-03-30 23:26:55 -0300

    Corrigindo bugs do Nav

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
-import { BarraDeBusca } from "./styled";
+import './Search.css'
 
 export default function Pesquisa() {
     return (
-        <BarraDeBusca>
+        <div>
             <form className="form">
                 <input type="text" placeholder="Busque por aqui"></input>
                 <span className="search">
                     <FaSearch />
                 </span>
             </form>
-        </BarraDeBusca>
+        </div>
     )
 }
